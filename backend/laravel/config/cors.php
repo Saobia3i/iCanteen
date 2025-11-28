7eb8cf35c42f50c99a1b85ada17b9a2c679bc1c7
Saobia Tinni <islamsaobia@gmail.com>
2025-11-28 20:22:23 +0600

    Fix CORS config array

--- backend/laravel/config/cors.php
+++ backend/laravel/config/cors.php
@@ -1,4 +1,19 @@
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_origins' => ['https://i-canteen.vercel.app'],
-'allowed_methods' => ['*'],
-'allowed_headers' => ['*'],
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://i-canteen.vercel.app'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
